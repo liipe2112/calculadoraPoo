3350519f672cfd90f2fad6ce626e682db8b0b0d9
liipe2112 <lipe_amorim10@hotmail.com>
2023-02-27 15:57:54 -0300

    Criação da classe calculadora

---
+++ Calculadora.java
@@ -0,0 +1,15 @@
+package calculadora;
+
+public class Calculadora {
+	private double numero1;
+	private double numero2;
+	
+	public Calculadora(double num1, double num2) {
+		this.numero1 = num1;
+		this.numero2 = num2;
+	}
+	
+	public void setNumero1(double num1) {
+		this.numero1 = num1;
+	}
+}
