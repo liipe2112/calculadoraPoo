99063ff811b761a9c2ace3f2539846801a9651d4
liipe2112 <lipe_amorim10@hotmail.com>
2023-02-27 17:05:32 -0300

    Criação e elaboração da classe subtracao

---
+++ Subtracao.java
@@ -0,0 +1,27 @@
+package calculadora;
+
+public class Subtracao {
+	private double numero1;
+	private double numero2;
+	
+	public Subtracao(double num1, double num2) {
+		this.numero1 = num1;
+		this.numero2 = num2;
+	}
+	
+	public void setNumero1(double num1) {
+		this.numero1 = num1;
+	}
+	
+	public void setNumero2(double num2) {
+		this.numero2 = num2;
+	}
+	
+	public double getNumero1() {
+		return numero1;
+	}
+	
+	public double getNumero2() {
+		return numero2;
+	}
+}
