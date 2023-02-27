58721f55d29e194d5b6c20c1dfb611f4984d2b55
liipe2112 <lipe_amorim10@hotmail.com>
2023-02-27 16:45:52 -0300

    Criação da calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -12,4 +12,32 @@ public class Calculadora {
 	public void setNumero1(double num1) {
 		this.numero1 = num1;
 	}
+	
+	public void setNumero2(double num2) {
+		this.numero2 = num2;
+	}
+	
+	public double getNumero1() {
+		return this.numero1;
+	}
+	
+	public double getNumero2() {
+		return this.numero2;
+	}
+	
+	public double soma() {
+		return this.numero1 + this.numero2;
+	}
+	
+	public double subtracao() {
+		return this.numero1 - this.numero2;
+	}
+	
+	public double multiplicacao() {
+		return this.numero1 * this.numero2;
+	}
+	
+	public double divisao() {
+		return this.numero1 / this.numero2;
+	}
 }
